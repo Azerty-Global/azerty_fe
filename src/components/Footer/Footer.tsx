1701df81332d85e1e84d1c18ca66367987490e4b
zekiblue <hi.zekiblue@gmail.com>
2024-03-17 08:57:20 +0000

    :sparkles: Make ready to final

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { type FC } from "react";
 
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 import { BlockNumber } from "@/components/atomicComponents";
 
@@ -10,10 +11,47 @@ const Footer: FC = () => {
   return (
     <Box as="footer" p={"1rem"} position="sticky" bottom={0} zIndex={10} textAlign={"center"}>
       <Flex w={"full"} justifyContent={"space-between"}>
-        <Link href="https://github.com/Azerty-Global" target="_blank" rel="noopener noreferrer">
-          Azerty Global (c) 2024
-        </Link>
+        <Flex minW={"400px"}>
+          <Link href="https://github.com/Azerty-Global" target="_blank" rel="noopener noreferrer">
+            Azerty Global (c) 2024
+          </Link>
+        </Flex>
         <BlockNumber />
+        <Flex gap={4} alignItems={"baseline"} minH={6}>
+          <Flex gap={2}>
+            <Text>Frontend: </Text>
+            <Link href={"https://github.com/azerty-global/azerty_fe"}>
+              <FaGithub alignmentBaseline={"before-edge"} size={24} />
+            </Link>
+          </Flex>
+          <Flex gap={2}>
+            <Text fontSize={"sm"}>Smart Contracts: </Text>
+            <Link href={"https://github.com/azerty-global/azerty_sc"}>
+              <FaGithub alignmentBaseline={"before-edge"} size={24} />
+            </Link>
+          </Flex>
+
+          <Link href={"https://github.com/azerty-global/azerty_sc"}>
+            <Text
+              textStyle={{
+                textDecoration: "underline",
+              }}
+              textUnderlineOffset={2}
+              fontSize={"sm"}
+            >
+              Smart contracts are not audited.
+            </Text>
+            <Text
+              textStyle={{
+                textDecoration: "underline",
+              }}
+              textUnderlineOffset={2}
+              fontSize={"sm"}
+            >
+              Use at your own risk.
+            </Text>
+          </Link>
+        </Flex>
       </Flex>
     </Box>
   );
